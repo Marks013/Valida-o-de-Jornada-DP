d7caef1f3372b5772fa1c7c7af8ba37e4603b72a
Marks013 <samuel14734@hotmail.com.br>
2026-01-09 11:27:24 -0300

    Add files via upload

--- src/ValidadorJornada/Converters/Converters.cs
+++ src/ValidadorJornada/Converters/Converters.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
@@ -63,6 +63,27 @@ namespace ValidadorJornada.Converters
         {
             if (value is bool boolValue)
                 return !boolValue;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Inverte bool e converte para Visibility (true → Collapsed, false → Visible)
+    /// Usado para mostrar controles quando checkbox está DESATIVADO
+    /// </summary>
+    public class InverseBoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+                return boolValue ? Visibility.Collapsed : Visibility.Visible;
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility)
+                return visibility != Visibility.Visible;
             return false;
         }
     }
